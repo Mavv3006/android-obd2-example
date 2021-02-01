5d2edd309022fd62144a1a4f903eafdb0e524a89
Mavv3006 <52504463+Mavv3006@users.noreply.github.com>
2021-02-01 12:07:52 +0100

    added some comments

--- app/src/main/java/de/deuschle/androidodb2example/Streams/BleOutputStream.java
+++ app/src/main/java/de/deuschle/androidodb2example/Streams/BleOutputStream.java
@@ -23,13 +23,25 @@ public class BleOutputStream extends OutputStream {
     }
 
     @Override
-    public void write(int i) throws IOException {
+    public void write(int i) {
         this.valueToWrite.write((Integer.valueOf(i)).byteValue());
     }
 
     @Override
-    public void flush() throws IOException {
+    public void write(@NonNull byte[] b, int off, int len) {
+        this.valueToWrite.write(b, off, len);
+    }
+
+    @Override
+    public void flush() {
         this.bleService.writeValue(valueToWrite.toByteArray());
         valueToWrite.reset();
+    }
+
+    /**
+     * Closing a {@code BleOutputStream} has no effect.
+     */
+    @Override
+    public void close() {
     }
 }
