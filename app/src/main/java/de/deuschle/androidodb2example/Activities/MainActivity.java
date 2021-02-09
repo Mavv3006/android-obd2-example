a38d73372e640bdc13408aa726b199d15bc4097d
Mavv3006 <52504463+Mavv3006@users.noreply.github.com>
2021-02-09 17:53:37 +0100

    added styling and MainActivity

---
+++ app/src/main/java/de/deuschle/androidodb2example/Activities/MainActivity.java
@@ -0,0 +1,45 @@
+package de.deuschle.androidodb2example.Activities;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import de.deuschle.androidodb2example.R;
+
+public class MainActivity extends AppCompatActivity {
+    Button disconnectButton;
+    Button connectButton;
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.main_toolbar);
+        toolbar.setSubtitle(getString(R.string.bluetooth_connection_status) + " " + getString(R.string.bluetooth_connection_disconnected));
+        setSupportActionBar(toolbar);
+
+        connectButton = findViewById(R.id.main_button_connect);
+        disconnectButton = findViewById(R.id.main_button_disconnect);
+
+        connectButton.setOnClickListener(view -> {
+            // TODO: connect to BLE device
+            toggleButtons();
+            toolbar.setSubtitle(getString(R.string.bluetooth_connection_status) + " " + getString(R.string.bluetooth_connection_connected));
+        });
+
+        disconnectButton.setOnClickListener(view -> {
+            // TODO: disconnect with BLE device
+            toggleButtons();
+            toolbar.setSubtitle(getString(R.string.bluetooth_connection_status) + " " + getString(R.string.bluetooth_connection_disconnected));
+        });
+    }
+
+    private void toggleButtons() {
+        disconnectButton.setEnabled(!disconnectButton.isEnabled());
+        connectButton.setEnabled(!connectButton.isEnabled());
+    }
+}
