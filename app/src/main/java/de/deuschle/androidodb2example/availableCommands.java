40c10e829db02db936497bd2832c612239bd1ba6
Tim Suchy <timsuchy98@gmail.com>
2021-03-29 22:39:39 +0200

    availableCommands optimiert & tests erstellt

--- app/src/main/java/de/deuschle/androidodb2example/availableCommands.java
+++ app/src/main/java/de/deuschle/androidodb2example/availableCommands.java
@@ -1,83 +1,64 @@
-import java.lang.reflect.Array;
+package de.deuschle.androidodb2example;
+
 import java.math.BigInteger;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 
 public class availableCommands {
 
-    private static int offset;
-    private static HashMap<String, Boolean> availabilityMap;
+    private final HashMap<String, Boolean> availabilityMap = new HashMap<>();
 
-
-    private static String hexToBin(String s) {
-        return new BigInteger(s, 16).toString(2);
-    }
-
-    private static String hexToBin2(String s) {
-        return String.format("%32S", Integer.toBinaryString(Integer.parseInt(s.replaceAll(" ", "").substring(4), 16))).replaceAll(" ", "0");
+    // Umwandlung von Hexadezimal zu Binär: String darf keine Leerzeichen enthalten
+    protected static String hexToBin(String s) {
+        return String.format("%32S", new BigInteger(s, 16).toString(2)).replaceAll(" ", "0");
     }
 
     private static int hexToDec(String s) {
         return Integer.parseInt(s, 16);
     }
 
-    private static void print(String string) {
-        System.out.println(string);
-    }
-
     public static boolean getAvailability(String PID, String availabilityList) throws Exception {
         // Leerzeichen und die ersten zwei Bytes entfernen
-        String availableCommandsInput = availabilityList.replaceAll(" ", "").substring(2);
+        String availableCommandsInput = availabilityList.replaceAll(" ", "");
 
         // Range sind nächsten zwei Bytes
-        String range = availableCommandsInput.substring(0,2);
-        availableCommandsInput = availableCommandsInput.substring(2);
+        String range = availableCommandsInput.substring(2,4);
 
         if (hexToDec(PID) <= (hexToDec(range)) || hexToDec(PID) > (hexToDec(range)) + 32) {
             throw new Exception("Befehl nicht in der Liste vorhanden");
         }
 
         // Hexadezimalen String in Binären String übersetzen
-        String availableCommandsOutput = hexToBin(availableCommandsInput);
+        String availableCommandsOutput = hexToBin(availableCommandsInput.substring(4));
 
         // Befehl an entsprechender Stelle heraussuchen und ausgeben
         if (hexToDec(PID)%32 != 0) {
-            if (availableCommandsOutput.charAt((hexToDec(PID)%32)-1) == '1') {
-                return true;
-            } else {
-                return false;
-            }
-        } else {
-            if (availableCommandsOutput.charAt((hexToDec(PID)-1)%32) == '1') {
-                return true;
-            } else {
-                return false;
-            }
+            return availableCommandsOutput.charAt((hexToDec(PID) % 32) - 1) == '1';
         }
+        return availableCommandsOutput.charAt((hexToDec(PID) - 1) % 32) == '1';
+
     }
 
-    public static HashMap getAvailabilityMap(ArrayList<String> PIDAvailabilityList, int ID) throws Exception {
+    public static HashMap<String, Boolean> getAvailabilityMap(ArrayList<String> PIDAvailabilityList, int ID)  {
+        return new availableCommands().getInternalAvailabilityMap(PIDAvailabilityList, ID);
+    }
 
+    private HashMap<String, Boolean> getInternalAvailabilityMap(ArrayList<String> PIDAvailabilityList, int ID) {
         // Leerzeichen und die ersten zwei Bytes entfernen
-        String availableCommandsInput = PIDAvailabilityList.get(ID).replaceAll(" ", "").substring(2);
+        String availableCommandsInput = PIDAvailabilityList.get(ID).replaceAll(" ", "");
 
         // Range sind nächsten zwei Bytes
-        int range = Integer.parseInt(availableCommandsInput.substring(0,2), 16);
-        availableCommandsInput = availableCommandsInput.substring(2);
+        int range = Integer.parseInt(availableCommandsInput.substring(2,4), 16);
 
         // Hexadezimaler Input in binären String übersetzen
-        String availableCommandsInputCharArray = hexToBin(availableCommandsInput);
-        print("availableCommandsInputCharArray: " + availableCommandsInputCharArray);
+        String availableCommandsInputCharArray = hexToBin(availableCommandsInput.substring(4));
 
-        for (int i = 0; i < availableCommandsInputCharArray.toString().length() ; i++) {
-            if (availableCommandsInputCharArray.charAt(i) == '1') {
-                availabilityMap.put(String.format("%02X", i+range+1), true);
-            }
-            if (availableCommandsInputCharArray.charAt(i) == '0') {
-                availabilityMap.put(String.format("%02X", i+range+1), false);
-            }
+        for (int i = 0; i < availableCommandsInputCharArray.length() ; i++) {
+            availabilityMap.put(String.format("%02X", i+range+1), availableCommandsInputCharArray.charAt(i) == '1');
         }
         if (availableCommandsInputCharArray.charAt(availableCommandsInputCharArray.length() - 1) == '1') {
-            return getAvailabilityMap(PIDAvailabilityList, ++ID);
+            return getInternalAvailabilityMap(PIDAvailabilityList, ++ID);
         }
         return availabilityMap;
     }
